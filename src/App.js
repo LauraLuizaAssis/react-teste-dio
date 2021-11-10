0b298b55b75a1f4785d86c4e9bc75b74fd859fd8
LauraAssis <laurinha449@gmail.com>
2021-11-10 00:30:18 -0300

    commit react-teste dio

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Item from './components/Item';
+import Card from './components/Card';
+
+const App = () => {
+  return(
+    <div>
+        <h1>Laura Assis</h1>
+        <ul>
+            <Item>
+              Item 1
+            </Item>
+            <Item>
+              Item 2
+            </Item>
+            <Item>
+              Item 3
+            </Item>
+        </ul>
+        <Card />
+    </div>
+    
+  )
+}
+
+export default App;
+
